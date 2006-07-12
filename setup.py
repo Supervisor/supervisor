bfca9e63b6655878c4c468fb16bc76230b820f44
Chris McDonough <chrism@plope.com>
2006-07-12 19:32:30 +0000

    Make setup.py sdist work.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if not maj[0] >= '2' and minor[0] >= '3':
 
 from distutils.core import setup
 
-setup(
+dist = setup(
     name = 'supervisor',
     version = "2.0",
     description = "A system for controlling process state under UNIX",
@@ -25,5 +25,19 @@ setup(
     packages = ['supervisor', 'supervisor.medusa', 'supervisor.meld3',
                 'supervisor.meld3.elementtree'],
     package_dir = {'supervisor':'.'},
+    # package_data doesn't work under 2.3
     package_data= {'supervisor':['ui/*.gif', 'ui/*.css', 'ui/*.html']},
     )
+
+if __name__ == '__main__':
+    # if pre-2.4 distutils was a joke, i suspect nobody laughed
+    if minor[0] <= '3':
+        if 'install' in sys.argv:
+            from distutils import dir_util
+            import os
+            pkg_dir = dist.command_obj['install'].install_purelib
+            for dirname in ['ui']:
+                dir_util.copy_tree(
+                    os.path.join(dirname),
+                    os.path.join(pkg_dir, 'supervisor',  dirname)
+                    )
