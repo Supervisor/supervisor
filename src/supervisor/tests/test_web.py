aa7c81f3254c3d4ec11d06a6ab02a4647a7e8f73
Chris McDonough <chrism@plope.com>
2007-08-22 23:18:27 +0000

    Get a place to start to track meld3 memory leaks.

--- src/supervisor/tests/test_web.py
+++ src/supervisor/tests/test_web.py
@@ -2,6 +2,7 @@ import sys
 import unittest
 
 from supervisor.tests.base import DummySupervisor
+from supervisor.tests.base import DummyRequest
 
 class StatusViewTests(unittest.TestCase):
     def _getTargetClass(self):
@@ -18,6 +19,27 @@ class StatusViewTests(unittest.TestCase):
         context.template = 'ui/status.html'
         view = self._makeOne(context)
         self.assertRaises(ValueError, view.make_callback, 'process', None)
+
+    def test_render_noaction(self):
+        context = DummyContext()
+        context.supervisord = DummySupervisor()
+        context.template = 'ui/status.html'
+        context.request = DummyRequest('/foo', [], '', '')
+        context.response = {}
+        view = self._makeOne(context)
+        data = view.render()
+        self.assertTrue(data.startswith('<!DOCTYPE html PUBLIC'), data)
+
+    def test_render_refresh(self):
+        context = DummyContext()
+        context.supervisord = DummySupervisor()
+        context.template = 'ui/status.html'
+        context.request = DummyRequest('/foo', [], '?action=refresh', '')
+        context.response = {}
+        view = self._makeOne(context)
+        data = view.render()
+        from supervisor.http import NOT_DONE_YET
+        self.assertTrue(data is NOT_DONE_YET, data)
 
 class DummyContext:
     pass
