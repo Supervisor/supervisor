06263f44ea44010d8175cb2ad135261ccc5e7acd
Chris McDonough <chrism@plope.com>
2007-08-22 03:20:59 +0000

    Also fragrant is the smell of attempting to remain compatible with four-year-old Python versions.

--- src/supervisor/tests/__init__.py
+++ src/supervisor/tests/__init__.py
@@ -1,1 +1,14 @@
 # this is a package
+from unittest import TestCase
+def assertTrue(self, value, extra=None):
+    if not value:
+        raise AssertionError(extra)
+def assertFalse(self, value, extra=None):
+    if value:
+        raise AssertionError(extra)
+
+if not hasattr(TestCase, 'assertTrue'): # Python 2.3.3
+    TestCase.assertTrue = assertTrue
+
+if not hasattr(TestCase, 'assertFalse'): # Pytthon 2.3.3
+    TestCase.assertFalse = assertFalse
