8bf99338845fde324ecf424d9f97a2f2a04fe551
btubbs <brent.tubbs@gmail.com>
2012-12-17 16:30:43 -0800

    add events for adding/removing process groups

--- supervisor/events.py
+++ supervisor/events.py
@@ -151,6 +151,18 @@ class ProcessStateStoppedEvent(ProcessStateEvent):
     def get_extra_values(self):
         return [('pid', self.process.pid)]
 
+class ProcessGroupAddedEvent(Event):
+    def __init__(self, group):
+        self.group = group
+    def __str__(self):
+        return self.group
+
+class ProcessGroupRemovedEvent(Event):
+    def __init__(self, group):
+        self.group = group
+    def __str__(self):
+        return self.group
+
 class TickEvent(Event):
     """ Abstract """
     def __init__(self, when, supervisord):
@@ -196,6 +208,8 @@ class EventTypes:
     TICK_5 = Tick5Event
     TICK_60 = Tick60Event
     TICK_3600 = Tick3600Event
+    PROCESS_GROUP_ADDED = ProcessGroupAddedEvent
+    PROCESS_GROUP_REMOVED = ProcessGroupRemovedEvent
 
 def getEventNameByType(requested):
     for name, typ in EventTypes.__dict__.items():
