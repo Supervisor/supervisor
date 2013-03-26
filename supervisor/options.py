dd26c6c057c40962b49676d5d5fc48f9d83e1974
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-03-26 13:05:59 +0200

    Sort the included files
    Conflicts:
    	supervisor/options.py

--- supervisor/options.py
+++ supervisor/options.py
@@ -567,7 +567,7 @@ class ServerOptions(Options):
                     self.parse_warnings.append(
                         'No file matches via include "%s"' % pattern)
                     continue
-                for filename in filenames:
+                for filename in sorted(filenames):
                     self.parse_warnings.append(
                         'Included extra file "%s" during parsing' % filename)
                     try:
