f8f6a83ff8eac2ec80ff6e3e500c45d290d46b6f
dongweiming <ciici1234@hotmail.com>
2013-11-20 23:22:15 +0800

    reconstruction states.py

--- supervisor/states.py
+++ supervisor/states.py
@@ -1,5 +1,13 @@
 # This module must not depend on any other non-stdlib module to prevent
 # circular import problems.
+from functools import partial
+
+
+def getState(cls, code):
+    for statename in cls.__dict__:
+        if getattr(cls, statename) == code:
+            return statename
+
 
 class ProcessStates:
     STOPPED = 0
@@ -21,11 +29,8 @@ RUNNING_STATES = (ProcessStates.RUNNING,
                   ProcessStates.STARTING)
 
 
+getProcessStateDescription = partial(getState, ProcessStates)
 
-def getProcessStateDescription(code):
-    for statename in ProcessStates.__dict__:
-        if getattr(ProcessStates, statename) == code:
-            return statename
 
 class SupervisorStates:
     FATAL = 2
@@ -33,10 +38,8 @@ class SupervisorStates:
     RESTARTING = 0
     SHUTDOWN = -1
 
-def getSupervisorStateDescription(code):
-    for statename in SupervisorStates.__dict__:
-        if getattr(SupervisorStates, statename) == code:
-            return statename
+
+getSupervisorStateDescription = partial(getState, SupervisorStates)
 
 
 class EventListenerStates:
@@ -45,8 +48,4 @@ class EventListenerStates:
     ACKNOWLEDGED = 30 # the event listener processed an event
     UNKNOWN = 40 # the event listener is in an unknown state
 
-def getEventListenerStateDescription(code):
-    for statename in EventListenerStates.__dict__:
-        if getattr(EventListenerStates, statename) == code:
-            return statename
-
+getEventListenerStateDescription = partial(getState, EventListenerStates)
