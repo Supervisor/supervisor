aa7d40e2949043ece15f972a23b28055c687f6fa
Stefan Friesel <sf@cloudcontrol.de>
2013-12-17 15:47:34 +0100

    Parse env configuration in posix mode
    Fixes #328
    
    This allows (escaped) quotes in the values as well as empty
    values.

--- supervisor/tests/test_datatypes.py
+++ supervisor/tests/test_datatypes.py
@@ -115,6 +115,21 @@ class DatatypesTest(unittest.TestCase):
         expected = {'foo': 'bar,baz', 'baz': 'q,ux'}
         self.assertEqual(actual, expected)
 
+    def test_dict_of_key_value_pairs_handles_newlines_inside_quotes(self):
+        actual = datatypes.dict_of_key_value_pairs('foo="a\nb\nc"')
+        expected = {'foo': 'a\nb\nc'}
+        self.assertEqual(actual, expected)
+
+    def test_dict_of_key_value_pairs_handles_quotes_inside_quotes(self):
+        actual = datatypes.dict_of_key_value_pairs('foo="\'\\""')
+        expected = {'foo': '\'"'}
+        self.assertEqual(actual, expected)
+
+    def test_dict_of_key_value_pairs_handles_empty_inside_quotes(self):
+        actual = datatypes.dict_of_key_value_pairs('foo=""')
+        expected = {'foo': ''}
+        self.assertEqual(actual, expected)
+
     def test_dict_of_key_value_pairs_handles_unquoted_non_alphanum(self):
         actual = datatypes.dict_of_key_value_pairs(
             'HOME=/home/auser,FOO=/.foo+(1.2)-_/,'
