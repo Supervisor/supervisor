Supervisor/supervisor

commit 332972ecc6255dc92dda9cd346ad4766834c6aa1

handle stopProcessGroup error.

supervisor/supervisorctl.py
@@ -746,10 +746,16 @@ class DefaultControllerPlugin(ControllerPluginBase):
             for name in names:
                 group_name, process_name = split_namespec(name)
                 if process_name is None:
-                    results = supervisor.stopProcessGroup(group_name)
-                    for result in results:
-                        result = self._stopresult(result)
-                        self.ctl.output(result)
+                    try:
+                        results = supervisor.stopProcessGroup(group_name)
+                        for result in results:
+                            result = self._stopresult(result)
+                            self.ctl.output(result)
+                    except xmlrpclib.Fault, e:
+                        error = self._startresult({'status': e.faultCode,
+                                                   'name': name,
+                                                   'description': e.faultString})
+                        self.ctl.output(error)
                 else:
                     try:
                         result = supervisor.stopProcess(name)

commit 60914473419d900e6ba56f97087cd73af855b9dd

Fix bug introduced in 332972ecc6255dc92dda9cd346ad4766834c6aa1 where do_stop() called _stopresult()

supervisor/supervisorctl.py
@@ -752,10 +752,10 @@ class DefaultControllerPlugin(ControllerPluginBase):
                             result = self._stopresult(result)
                             self.ctl.output(result)
                     except xmlrpclib.Fault, e:
-                        error = self._startresult({'status': e.faultCode,
-                                                   'name': process_name,
-                                                   'group': group_name,
-                                                   'description': e.faultString})
+                        error = self._stopresult({'status': e.faultCode,
+                                                  'name': process_name,
+                                                  'group': group_name,
+                                                  'description': e.faultString})
                         self.ctl.output(error)
                 else:
                     try: