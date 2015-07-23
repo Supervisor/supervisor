7dcbc2c2adc5cf2edc42799cf1c26d2e865ed021
Mike Naberezny <mike@naberezny.com>
2015-07-23 13:27:07 -0700

    Remove unused compat aliases

--- supervisor/compat.py
+++ supervisor/compat.py
@@ -6,14 +6,12 @@ PY3 = sys.version>'3'
 if PY3: # pragma: no cover
     long = int
     basestring = str
-    unichr = chr
     raw_input = input
     class unicode(str):
         def __init__(self, string, encoding, errors):
             str.__init__(self, string)
     def as_bytes(s): return s if isinstance(s,bytes) else s.encode('utf8')
     def as_string(s): return s if isinstance(s,str) else s.decode('utf8')
-    from functools import reduce
 
 else: # pragma: no cover
     long = long
@@ -22,7 +20,6 @@ else: # pragma: no cover
     basestring = basestring
     def as_bytes(s): return s if isinstance(s, str) else s.encode('utf-8')
     def as_string(s): return s if isinstance(s, unicode) else s.decode('utf-8')
-    reduce = reduce
 
 def total_ordering(cls): # pragma: no cover
     """Class decorator that fills in missing ordering methods"""
