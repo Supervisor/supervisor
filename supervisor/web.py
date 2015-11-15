d542cde079ce1e5d7d48881ac4b1f8e9acd3c888
Mike Naberezny <mike@naberezny.com>
2015-11-14 20:02:29 -0800

    Show errors when stopping a process on the web interface

--- supervisor/web.py
+++ supervisor/web.py
@@ -335,9 +335,22 @@ class StatusView(MeldView):
                     return wrong
 
                 elif action == 'stop':
-                    callback = rpcinterface.supervisor.stopProcess(namespec)
+                    try:
+                        callback = rpcinterface.supervisor.stopProcess(
+                            namespec)
+                    except RPCError as e:
+                        def stoperr():
+                            return 'unexpected rpc fault [%d] %s' % (
+                                e.code, e.text)
+                        stoperr.delay = 0.05
+                        return stoperr
+
                     def stopprocess():
-                        result = callback()
+                        try:
+                            result = callback()
+                        except RPCError as e:
+                            return 'unexpected rpc fault [%d] %s' % (
+                                e.code, e.text)
                         if result is NOT_DONE_YET:
                             return NOT_DONE_YET
                         return 'Process %s stopped' % namespec
