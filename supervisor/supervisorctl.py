Supervisor/supervisor

commit e0138563a5ffbd834cfae7f11e73147c4286f671

clarify reread and update

supervisor/supervisorctl.py
@@ -1086,7 +1086,7 @@ class DefaultControllerPlugin(ControllerPluginBase):
         self.ctl.handle_error(message=message, fatal=fatal, code=code)
 
     def help_reread(self):
-        self.ctl.output("reread \t\t\tReload the daemon's configuration files")
+        self.ctl.output("reread \t\t\tReload the daemon's configuration files without add and remove")
 
     def do_add(self, arg):
         names = arg.split()
@@ -1199,8 +1199,8 @@ class DefaultControllerPlugin(ControllerPluginBase):
             log(gname, "added process group")
 
     def help_update(self):
-        self.ctl.output("update\t\t\tReload config and add/remove as necessary")
-        self.ctl.output("update all\t\tReload config and add/remove as necessary")
+        self.ctl.output("update\t\t\tReload config and add and remove as necessary, and will restart affected programs")
+        self.ctl.output("update all\t\tReload config and add and remove as necessary, and will restart affected programs")
         self.ctl.output("update <gname> [...]\tUpdate specific groups")
 
     def _clearresult(self, result):

commit bb0332fc36e2001066bac528a68eda55f5914fbc

Fix help broken in e0138563a5ffbd834cfae7f11e73147c4286f671 to make tests pass again

supervisor/supervisorctl.py
@@ -1086,7 +1086,7 @@ class DefaultControllerPlugin(ControllerPluginBase):
         self.ctl.handle_error(message=message, fatal=fatal, code=code)
 
     def help_reread(self):
-        self.ctl.output("reread \t\t\tReload the daemon's configuration files without add and remove")
+        self.ctl.output("reread \t\t\tReload the daemon's configuration files without add/remove")
 
     def do_add(self, arg):
         names = arg.split()
@@ -1199,8 +1199,8 @@ class DefaultControllerPlugin(ControllerPluginBase):
             log(gname, "added process group")
 
     def help_update(self):
-        self.ctl.output("update\t\t\tReload config and add and remove as necessary, and will restart affected programs")
-        self.ctl.output("update all\t\tReload config and add and remove as necessary, and will restart affected programs")
+        self.ctl.output("update\t\t\tReload config and add/remove as necessary, and will restart affected programs")
+        self.ctl.output("update all\t\tReload config and add/remove as necessary, and will restart affected programs")
         self.ctl.output("update <gname> [...]\tUpdate specific groups")
 
     def _clearresult(self, result):