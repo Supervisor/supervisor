34ea5ce3b82f5d37ec4273ab954bac99b29689f7
Mike Naberezny <mike@naberezny.com>
2018-12-22 13:39:57 -0800

    Fix indentation

--- supervisor/xmlrpc.py
+++ supervisor/xmlrpc.py
@@ -490,9 +490,9 @@ class SupervisorTransport(xmlrpclib.Transport):
             raise ValueError('Unknown protocol for serverurl %s' % serverurl)
 
     def close(self):
-      if self.connection:
-        self.connection.close()
-        self.connection = None
+        if self.connection:
+            self.connection.close()
+            self.connection = None
 
     def request(self, host, handler, request_body, verbose=0):
         if not self.connection:
