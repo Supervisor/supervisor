32498e902780c6dfd916b62846f4811eba712dd2
Trevor Foster <trevor.foster@hotmail.ca>
2019-12-02 01:19:01 -0500

    Fix duplicate short option

--- supervisor/options.py
+++ supervisor/options.py
@@ -449,7 +449,7 @@ class ServerOptions(Options):
         self.add("profile_options", "supervisord.profile_options",
                  "", "profile_options=", profile_options, default=None)
         self.add("quiet", "supervisord.quiet",
-                 "q", "quiet", flag=1, default=0)
+                 "s", "quiet", flag=1, default=0)
         self.pidhistory = {}
         self.process_group_configs = []
         self.parse_criticals = []
