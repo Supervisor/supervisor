5f1b906ba80990c3113c06c20fdfed86eb09a9bb
Hassan Alshehri <alshehri.hass@gmail.com>
2021-04-21 07:27:36 +0300

    Adding the only supervisor dependency: python-json-logger

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if py_version < (2, 7):
 elif (3, 0) < py_version < (3, 4):
     raise RuntimeError('On Python 3, Supervisor requires Python 3.4 or later')
 
-requires = []
+requires = ["python-json-logger==2.0.1"]
 tests_require = []
 if py_version < (3, 3):
     tests_require.append('mock<4.0.0.dev0')
