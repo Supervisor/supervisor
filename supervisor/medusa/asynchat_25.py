a1f7aab629cbfdd647221b068a64c377d7ebf661
Qiao Nan <463553109@qq.com>
2021-04-24 16:04:26 +0800

    fix bug that num_send is bigger than obs
    when I add chinese words to config file and show to the web page,  it always lost something in html-string make webpage shows strange in chrome.
    after debug,  I found that num_sent can bigger than obs,  as num_sent is 4206 but obs is 4096. it means  4096 bytes send in the first time, but in the second time, it send from the  position 4207. that's the reason why something lost in the webpage.

--- supervisor/medusa/asynchat_25.py
+++ supervisor/medusa/asynchat_25.py
@@ -219,7 +219,8 @@ class async_chat (asyncore.dispatcher):
             try:
                 num_sent = self.send (self.ac_out_buffer[:obs])
                 if num_sent:
-                    self.ac_out_buffer = self.ac_out_buffer[num_sent:]
+                    real_send_num = num_sent if num_sent < obs else obs
+                    self.ac_out_buffer = self.ac_out_buffer[real_send_num:]
 
             except socket.error:
                 self.handle_error()
