c88522438e9fe66b35a88913b2e4280f72ab7b78
Michael Hammann <michael.hammann@agile-robots.com>
2021-12-09 16:36:22 +0100

    feat: added functions to check whether two nodes in a Digraph are connected

--- supervisor/graphutils.py
+++ supervisor/graphutils.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
  
 class Graph():
+    """ Class to save and analyse a directed graph
+    """
     def __init__(self,vertices):
         self.graph = defaultdict(list)
         self.V = vertices
@@ -9,7 +11,7 @@ class Graph():
         self.graph[u].append(v)
 
     def cyclic(self):
-        """Return True if the directed graph has a cycle.
+        """ Return True if the directed graph has a cycle.
         The graph must be represented as a dictionary mapping vertices to
         iterables of neighbouring vertices. For example:
 
@@ -34,3 +36,21 @@ class Graph():
             return False
 
         return any(visit(v) for v in self.graph)
+
+
+    def connected(self, start_node, end_node):
+        """ Check whether two nodes are connected, and if the
+        start_node comes before the end_node.
+
+        """
+        visited = set()
+
+        return self._explore_graph_from(start_node, end_node, visited)
+
+    def _explore_graph_from(self, start_node, end_node, visited):
+        """ Check if end_node comes after start_node and if they are connected
+        """
+        for neighbour in self.graph.get(start_node, ()):
+            visited.add(neighbour)
+            self._explore_graph_from(neighbour, end_node, visited)
+        return end_node in visited
