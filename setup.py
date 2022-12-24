d15dab25bf22073a4f45f9e8beca5163a4676b2c
Mike Naberezny <mike@naberezny.com>
2022-12-24 13:55:08 -0800

    Add Python 3.11 classifier

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
 ]
 
 version_txt = os.path.join(here, 'supervisor/version.txt')
