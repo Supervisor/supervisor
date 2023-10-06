182ae087d46ee726aff30c8f42fa0f242daed694
Mike Naberezny <mike@naberezny.com>
2023-10-06 09:36:49 -0700

    Add Python 3.12

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
 ]
 
 version_txt = os.path.join(here, 'supervisor/version.txt')
